28e85354b562c04527623990540bb632a3a13087
shinnosuke0522 <shinnosuke.dev@gmail.com>
2026-03-26 23:50:31 +0900

    remove: default app package

--- src/test/kotlin/com/github/shinnosuke0522/flight/checker/TestFlightCheckerApplication.kt
+++
@@ -1,9 +0,0 @@
-package com.github.shinnosuke0522.flight.checker
-
-import org.springframework.boot.fromApplication
-import org.springframework.boot.with
-
-
-fun main(args: Array<String>) {
-	fromApplication<FlightCheckerApplication>().with(TestcontainersConfiguration::class).run(*args)
-}
--- src/test/kotlin/com/github/shinnosuke0522/flight/checker/TestcontainersConfiguration.kt
+++
@@ -1,6 +0,0 @@
-package com.github.shinnosuke0522.flight.checker
-
-import org.springframework.boot.test.context.TestConfiguration
-
-@TestConfiguration(proxyBeanMethods = false)
-class TestcontainersConfiguration
