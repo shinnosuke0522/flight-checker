cfab3a7adf16e0c421aaa1df2fd549e6631e909f
shinnosuke0522 <shinnosuke.dev@gmail.com>
2026-06-13 20:18:03 +0900

    remove: travel schema and merge all schema files

--- adapter/inbound/graphql-adaptor/src/main/kotlin/com/shinnosuke0522/flight/checker/adapter/inbound/graphql/resolver/TravelQueryResolver.kt
+++
@@ -1,8 +0,0 @@
-package com.shinnosuke0522.flight.checker.adapter.inbound.graphql.resolver
-
-import com.shinnosuke0522.flight.checker.adapter.inbound.graphql.model.Travel
-
-interface TravelQueryResolver {
-    suspend fun travel(id: String): Travel
-    suspend fun travels(ids: List<String>): List<Travel>
-}
