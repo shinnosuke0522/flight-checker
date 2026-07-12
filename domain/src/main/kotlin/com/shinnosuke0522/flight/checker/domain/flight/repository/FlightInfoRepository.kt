89895a92777eec02a271af5f2e9cc2be330d88ab
shinnosuke0522 <shinnosuke.dev@gmail.com>
2026-07-13 00:00:23 +0900

    add: repository for aggregates

---
+++ domain/src/main/kotlin/com/shinnosuke0522/flight/checker/domain/flight/repository/FlightInfoRepository.kt
@@ -0,0 +1,9 @@
+package com.shinnosuke0522.flight.checker.domain.flight.repository
+
+import com.shinnosuke0522.flight.checker.domain.flight.model.FlightInfo
+import com.shinnosuke0522.flight.checker.domain.shared.primitive.FlightIdentity
+
+interface FlightInfoRepository {
+    suspend fun findByFlightIdentity(flightIdentity: FlightIdentity): FlightInfo?
+    suspend fun save(flight: FlightInfo)
+}
