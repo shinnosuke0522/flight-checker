24e809ac0651174bbef9321a08e8f1800cb222a7
shinnosuke0522 <shinnosuke.dev@gmail.com>
2026-07-24 21:50:37 +0900

    Refactor: Rename properties in AwsProps instead of using @Name

--- libs/aws/src/main/kotlin/com/shinnosuke0522/flight/checker/common/aws/config/AwsProps.kt
+++ libs/aws/src/main/kotlin/com/shinnosuke0522/flight/checker/common/aws/config/AwsProps.kt
@@ -1,7 +1,6 @@
 package com.shinnosuke0522.flight.checker.common.aws.config
 
 import org.springframework.boot.context.properties.ConfigurationProperties
-import org.springframework.boot.context.properties.bind.Name
 import software.amazon.awssdk.auth.credentials.AwsBasicCredentials
 import software.amazon.awssdk.auth.credentials.AwsCredentials
 import software.amazon.awssdk.regions.Region
@@ -9,10 +8,10 @@ import software.amazon.awssdk.regions.Region
 @ConditionalOnAwsEnabled
 @ConfigurationProperties(prefix = AwsConfigConstants.PREFIX)
 data class AwsProps(
-    @Name("region") private val awsRegion: String,
-    @Name("access-key") private val awsAccessKey: String,
-    @Name("secret-key") private val awsSecretKey: String,
+    private val region: String,
+    private val accessKey: String,
+    private val secretKey: String,
 ) {
-    val region: Region = Region.of(awsRegion)
-    val credentials: AwsCredentials = AwsBasicCredentials.create(awsAccessKey, awsSecretKey)
+    val awsRegion: Region = Region.of(region)
+    val credentials: AwsCredentials = AwsBasicCredentials.create(accessKey, secretKey)
 }
